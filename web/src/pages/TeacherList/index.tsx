13f0118a5f149ec04e16fe3cd43a91368481de85
Lucas Feng <lucasfeng83@gmail.com>
2021-03-05 01:20:14 -0300

    Add/Pages + CSS

---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import PageHeader from '../../components/pageHeader';
+
+import './styles.css';
+
+function TeacherList() {
+  return (
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Estes são os proffys disponíveis.">
+        Teste
+      </PageHeader>
+    </div>
+  )
+}
+
+export default TeacherList;
